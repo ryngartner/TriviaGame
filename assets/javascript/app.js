9ea07a8760a61f9f1e62c8092be46a1fac5a6caa
Ryan Gartner <ryanjgartner@gmail.com>
2019-01-10 22:40:43 -0600

    TriviaGame

---
+++ assets/javascript/app.js
@@ -0,0 +1,44 @@
+// Begin Javascript
+
+
+// $("#start").click(function(){
+//     alert("Yay!");
+// })
+
+var score = 0
+
+var quiz = [{
+    question: "Kramer kramer kramer",
+    panswer: ["Newman","Jerry","Elaine"],
+    answer: 1 },
+{question: "Jerry Jerry Jerry",
+panswer: ["Soup Nazi","Jerry","Elaine"],
+answer: 0 }
+]
+var questionNumber;
+$(quiz).each(function(i,e){
+    questionNumber=i;
+    console.log(e)
+    console.log(i)
+for(i = 0;i < 4;i++){
+    $("#possibleAnswers").append('<label class="radio-inline"><input type="radio" name="optradio' + questionNumber + '" value =' + (i)+'>' + (e.panswer[i]) + '</label>')
+}})
+
+$("#submit").click(function(){
+    var radioValue = $("input[name='optradio']:checked").val();
+   
+    if (parseInt(radioValue) === quiz[0].answer){
+        score ++;
+    }
+    else {
+        console.log("Incorrect");
+    }
+    $("#results").text(score);
+})
+
+
+
+
+
+
+
